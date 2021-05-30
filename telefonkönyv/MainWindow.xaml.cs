957d8dba075660c2ade9c4c97d80d90e62253ae4
szilagyiszabolcs <70943535+szilagyiszabolcs@users.noreply.github.com>
2021-05-30 16:31:38 +0200

    Add files via upload
    újdonságok 2021.05.30

--- telefonkönyv/MainWindow.xaml.cs
+++ telefonkönyv/MainWindow.xaml.cs
@@ -26,22 +26,23 @@ namespace telefonkönyv
         public MainWindow()
         {
             InitializeComponent();
-            contacts = JsonConvert.DeserializeObject<List<Contact>>(File.ReadAllText("contacts.json"));
-            ShowContacts();
+            try
+            {
+                contacts = JsonConvert.DeserializeObject<List<Contact>>(File.ReadAllText("contacts.json"));
+                foreach (var item in contacts)
+                {
+                    adatok.Items.Add(item);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+            }
         }
 
         static void Save()
         {
             string json = JsonConvert.SerializeObject(contacts, Formatting.Indented);
             File.WriteAllText("contacts.json", json);
-        }
-
-        static void ShowContacts()
-        {
-            foreach (var item in contacts)
-            {
-                //listbox.Items.Add($"{item.Name} - {item.Number}");    MÉG NEM MŰKÖDIK
-            }
         }
 
         private void Add_Click(object sender, RoutedEventArgs e)
@@ -53,13 +54,84 @@ namespace telefonkönyv
                     Name = TextBoxName.Text,
                     Number = TextBoxNumber.Text
                 };
+
+                TextBoxName.Text = "";
+                TextBoxNumber.Text = "";
+
                 contacts.Add(c);
                 Save();
-                ShowContacts();
+                adatok.Items.Add(c);
             }
             else
             {
                 MessageBox.Show("A név vagy telefonszám hiányzik!", "HIBA", MessageBoxButton.OK);
+            }
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            Contact selected = (Contact)adatok.SelectedItem;
+            Contact modified = new Contact
+            {
+                Name = TextBoxName.Text,
+                Number = TextBoxNumber.Text
+            };
+
+            try
+            {
+                contacts[contacts.IndexOf(selected)] = modified;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("Nincs kiválasztott elem!", "HIBA", MessageBoxButton.OK);
+            }
+
+            adatok.SelectedItem = null;
+            TextBoxName.Text = "";
+            TextBoxNumber.Text = "";
+
+            Save();
+
+            adatok.Items.Clear();
+            foreach (var item in contacts)
+            {
+                adatok.Items.Add(item);
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxName.Text == "" || TextBoxNumber.Text == "")
+            {
+                MessageBox.Show("Nincs kiválasztott elem!", "HIBA", MessageBoxButton.OK);
+            }
+
+            Contact selected = (Contact)adatok.SelectedItem;
+            contacts.Remove(selected);
+
+            adatok.SelectedItem = null;
+            TextBoxName.Text = "";
+            TextBoxNumber.Text = "";
+
+            Save();
+
+            adatok.Items.Clear();
+            foreach (var item in contacts)
+            {
+                adatok.Items.Add(item);
+            }
+        }
+
+        private void Adatok_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            Contact selected = (Contact)adatok.SelectedItem;
+            try
+            {
+                TextBoxName.Text = selected.Name;
+                TextBoxNumber.Text = selected.Number;
+            }
+            catch (NullReferenceException)
+            {
             }
         }
     }
